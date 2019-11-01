3e9f5f3a669e949c46c721637cb26a1852f0e1ac
russaa <3603062+russaa@users.noreply.github.com>
2019-11-01 17:22:59 +0100

    FIX prefix config settings with plugin ID

--- src/util/SettingsUtils.ts
+++ src/util/SettingsUtils.ts
@@ -1,6 +1,7 @@
 
 import { ConfigurationManager } from 'mmir-lib';
 import { IAppSettings } from '../typings/app-settings.d';
+import { PLUGIN_ID } from '../consts';
 
 export interface AppSettingsConfigImpl extends IAppSettings {
   _conf: ConfigurationManager;
@@ -10,10 +11,10 @@ export function createConfigSettingsImpl(mmirConf: ConfigurationManager): IAppSe
   return {
     _conf: mmirConf,
     get(this: AppSettingsConfigImpl, key: string): Promise<any> {
-      return Promise.resolve(this._conf.get(key));
+      return Promise.resolve(this._conf.get([PLUGIN_ID, key]));
     },
     set(this: AppSettingsConfigImpl, key: string, value: any): Promise<any> {
-      return Promise.resolve(this._conf.set(key, value));
+      return Promise.resolve(this._conf.set([PLUGIN_ID, key], value));
     },
     remove(this: AppSettingsConfigImpl, key: string): Promise<any> {
       return this.set(key, null);
