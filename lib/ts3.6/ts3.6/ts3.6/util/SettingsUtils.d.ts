9cf3f47c3b504dfe9671312d0bb14fc3068ea1ee
russaa <3603062+russaa@users.noreply.github.com>
2020-01-29 21:39:21 +0100

    FIX creation of compatibility typings: prevent recursion into unmodified typings copy/dir

--- lib/ts3.6/ts3.6/ts3.6/util/SettingsUtils.d.ts
+++
@@ -1,6 +0,0 @@
-import { ConfigurationManager } from 'mmir-lib';
-import { IAppSettings } from '../typings/app-settings.d';
-export interface AppSettingsConfigImpl extends IAppSettings {
-    _conf: ConfigurationManager;
-}
-export declare function createConfigSettingsImpl(mmirConf: ConfigurationManager): IAppSettings;
--- lib/ts3.6/ts3.6/util/SubscriptionUtil.d.ts
+++
@@ -1,6 +0,0 @@
-import { Subscription } from 'rxjs';
-import { SpeechEventName, SpeechEventEmitter } from '../typings/mmir-ext-dialog.d';
-export declare class SubscriptionUtil {
-    static subscribe(eventEmitter: SpeechEventEmitter<any>, subscriptions: Array<SpeechEventName>, handler: any): Map<SpeechEventName, Subscription>;
-    static unsubscribe(speechEventSubscriptions: Map<SpeechEventName, Subscription>): void;
-}
