d0c95f059277daf16461a811593b2bcd0aff8656
russa <aaron.russ@dfki.de>
2020-08-13 19:04:10 +0200

    extend interface definition  for Speech/Reading indicator
     * added optional property visible
     * made event argument (show/toggle/startReading) more specific with interface definition Targetable

--- src/typings/speech-io-indicator.d.ts
+++ src/typings/speech-io-indicator.d.ts
@@ -1,10 +1,17 @@
 
+export interface Targetable {
+  target?: HTMLElement | EventTarget;
+  currentTarget?: HTMLElement | EventTarget;
+  [field: string]: any
+}
+
 export interface ISpeechInputIndicator {
 
   initialized: boolean;
+  visible?: boolean;
 
-  show(event: any, target?: HTMLElement): void;
-  toggle(event: any, target?: HTMLElement): void;
+  show(event: Targetable | any, target?: HTMLElement): void;
+  toggle(event: Targetable | any, target?: HTMLElement): void;
   hide(): void;
   ready(overlayTarget?: OverlayTarget) : Promise<void>;
 }
@@ -12,8 +19,9 @@ export interface ISpeechInputIndicator {
 export interface ISpeechOutputIndicator {
 
   initialized: boolean;
+  visible?: boolean;
 
-  startReading(event: any, target?: HTMLElement): void;
+  startReading(event: Targetable | any, target?: HTMLElement): void;
   stopReading(isLeaveOpen?: boolean): void;
 
   ready(overlayTarget?: OverlayTarget) : Promise<void>;
